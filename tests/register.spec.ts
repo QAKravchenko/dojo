8c150404536a501844377735b2faeaefa30d5a50
Svyatoslav <qakravchenko@gmail.com>
2026-09-21 16:32:37 +0300

    Created login tests

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,22 +1,56 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('http://104.168.59.50/articles');
-  await expect (page).toHaveURL('http://104.168.59.50/articles');
-  await page.getByTestId('nav-sign-up').click();
-  expect (page.getByTestId('nav-sign-up').isVisible());
-  await page.getByTestId('auth-username').fill('Svyatoslav');
-  expect (page.getByTestId('auth-username')).toHaveValue('Svyatoslav');
-  await page.getByTestId('auth-email').fill('qa1@gmail.com');
-  expect (page.getByTestId('auth-email')).toHaveValue('qa@gmail.com');
-  await page.getByTestId('auth-password').fill('Testing1');
-  expect (page.getByTestId('auth-password')).toHaveValue('Testing1');
-  await page.getByTestId('register-confirm-password').fill('Testing1');
-  expect (page.getByTestId('register-confirm-password')).toHaveValue('Testing1');
-  await page.getByTestId('register-source-course').check();
-  expect (page.getByTestId('register-source-course')).toBeChecked();
-  await page.getByTestId('register-newsletter').check();
-  expect (page.getByTestId('register-newsletter')).toBeChecked();
-  await page.getByTestId('register-terms').check();
-  expect (page.getByTestId('register-terms')).toBeChecked();
+test.describe('New user registration', () => {
+    test('Successful registration with valid data', async ({ page }) => {
+        const timestamp = Date.now();
+        const username = `user_${timestamp}`;
+        const email = `qa_${timestamp}@gmail.com`;
+
+        await page.goto('/articles');
+        await expect(page).toHaveURL(/\/articles$/);
+        await expect(page.getByTestId('nav-sign-up')).toBeVisible();
+        await page.getByTestId('nav-sign-up').click();
+        await expect(page).toHaveURL(/\/articles\/register/);
+
+        await page.getByTestId('auth-username').fill(username);
+        await page.getByTestId('auth-email').fill(email);
+        await page.getByTestId('auth-password').fill('Testing1');
+        await page.getByTestId('register-confirm-password').fill('Testing1');
+        await page.getByTestId('register-newsletter').check();
+        await page.getByTestId('register-terms').check();
+        await page.getByTestId('auth-submit').click();
+
+        await expect(page).toHaveURL(/\/articles$/);
+        await expect(page.getByTestId('nav-profile')).toBeVisible();
+    });
+
+    test('Registration with existed email', async ({page}) => {
+        await page.goto('/articles');
+        await expect(page).toHaveURL('/articles');
+        await page.getByTestId('nav-sign-up').click();
+        await expect(page).toHaveURL('/articles/register');
+
+        await page.getByTestId('auth-username').fill('Svyatoslav');
+        await page.getByTestId('auth-email').fill('qa@gmail.com');
+        await page.getByTestId('auth-password').fill('Testing1');
+        await page.getByTestId('register-confirm-password').fill('Testing1');
+        await page.getByTestId('register-terms').check();
+        await page.getByTestId('auth-submit').click();
+
+        await expect(page.getByTestId('error-messages')).toHaveText('body email або username вже зайняті');
+    })
+
+    test('Registration with empty "Email" field', async ({page}) => {
+        await page.goto('articles/register');
+        await expect(page).toHaveURL('articles/register');
+        
+        await page.getByTestId('auth-username').fill('Svyatoslav');
+        await page.getByTestId('auth-email').fill('');
+        await page.getByTestId('auth-password').fill('Testing1');
+        await page.getByTestId('register-confirm-password').fill('Testing1');
+        await page.getByTestId('register-terms').check();
+        await page.getByTestId('auth-submit').click();
+
+        await expect(page.getByTestId('error-messages')).toHaveText('email некоректний email');
+    })
 });
