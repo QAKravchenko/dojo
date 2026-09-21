4fdee83866dade87df9d39e82cff5e1a4a157c97
Svyatoslav <qakravchenko@gmail.com>
2026-09-21 16:55:38 +0300

    merging register and login files into auth file

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -39,7 +39,7 @@ test.describe('New user registration', () => {
         await page.getByTestId('auth-submit').click();
 
         await expect(page.getByTestId('error-messages')).toHaveText('body email або username вже зайняті');
-    })
+    });
 
     test('Registration with empty "Email" field', async ({page}) => {
         await page.goto('articles/register');
@@ -53,5 +53,43 @@ test.describe('New user registration', () => {
         await page.getByTestId('auth-submit').click();
 
         await expect(page.getByTestId('error-messages')).toHaveText('email некоректний email');
-    })
+    });
+});
+
+test.describe('Login', () => {
+    test('Successful login with existed user', async ({page}) => {
+        await page.goto('/articles/login');
+        await expect(page).toHaveURL('/articles/login');
+
+        await page.getByTestId('auth-email').fill('qakravchenko@gmail.com');
+        await page.getByTestId('auth-password').fill('Testing1');
+        await page.getByTestId('auth-submit').click();     
+
+        await expect(page).toHaveURL('/articles');
+        await expect(page.getByTestId('nav-profile')).toContainText('Svyatoslav');
+    });
+
+    test('Login with invalid password', async ({page}) => {
+        await page.goto('/articles/login');
+        await expect(page).toHaveURL('/articles/login');
+
+        await page.getByTestId('auth-email').fill('qakravchenko@gmail.com');
+        await page.getByTestId('auth-password').fill('testing1');
+        await page.getByTestId('auth-submit').click();
+
+        await expect(page.getByTestId('error-messages')).toHaveText('email or password неправильні');
+    });
+
+    test('Login with not registered user', async ({page}) => {
+        await page.goto('/articles/login');
+        await expect(page).toHaveURL('/articles/login');
+
+        await page.getByTestId('auth-email').fill('somebody@gmail.com');
+        await expect(page.getByTestId('auth-email')).toHaveValue('somebody@gmail.com');
+        await page.getByTestId('auth-password').fill('pass');
+        await expect (page.getByTestId('auth-password')).toHaveValue('pass');
+        await page.getByTestId('auth-submit').click();
+
+        await expect(page.getByTestId('error-messages')).toHaveText('email or password неправильні');
+    });
 });
